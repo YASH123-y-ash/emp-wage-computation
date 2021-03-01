4acd6eeb547e1eff8f7e1a673c84f7fa2534af90
Yash-y-ash <wardhan696@gmail.com>
2021-03-01 14:40:29 +0530

    Added:employee daily wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
-empCheck=$((RANDOM%2))
-IS_PRESENT=1
-if [ $empCheck -eq $IS_PRESENT ]
-then
-	echo "employee is Present"
-else
-	echo "Employee is absent"
-fi
+#! /bin/bash -x
 
+ EMP_RATE_PER_HR=20;
+ FULL_TIME_HRS=8;
+
+randomCheck=$((RANDOM%2))
+isPresent=1;
+if [ $isPresent -eq $randomCheck ]
+then
+         salary=$(( $EMP_RATE_PER_HR*$FULL_TIME_HRS))
+   else
+      salary=0;
+fi
+echo "Daily Wage of an Employee= " $salary
